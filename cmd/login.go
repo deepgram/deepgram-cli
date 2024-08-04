05834872b02216d950e41f40e5e6dd8107c02973
Luke Oliff <luke@lukeoliff.com>
2024-08-04 14:06:40 +0100

    feat: layout structure for web or key auth

--- cmd/login.go
+++ cmd/login.go
@@ -25,21 +25,17 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // loginCmd represents the login command
 var loginCmd = &cobra.Command{
 	Use:   "login",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("login called")
-	},
+	Short: "Log in a user",
+	Long: `Logs a user into Deepgram. Supports browser-based and API key. 
+Defaults to using browser-based authentication.
+	`,
+	Run: login,
 }
 
 func init() {
@@ -53,5 +49,52 @@ func init() {
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// loginCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	loginCmd.Flags().StringP("dg-key", "k", "", "Configure the CLI with your Deepgram API key")
+	viper.BindPFlag("dg-key", loginCmd.Flags().Lookup("dg-key"))
+}
+
+func login(cmd *cobra.Command, args []string) {
+	var (
+		apiKey string = viper.GetString("dg-key")
+
+		str string
+		err error
+	)
+
+	cmd.Context()
+
+	switch {
+	case apiKey != "":
+		str, err = configureAuth(cmd, apiKey)
+	default:
+		str, err = webAuth(cmd)
+	}
+
+	fmt.Print(str, err)
+}
+
+func configureAuth(cmd *cobra.Command, apiKey string) (string, error) {
+	var (
+		url string = "https://community.deepgram.com/auth/cli"
+		err error
+	)
+
+	if err != nil {
+		return "", err
+	}
+
+	return url, nil
+}
+
+func webAuth(cmd *cobra.Command) (string, error) {
+	var (
+		apiKey string = "0987654321"
+		err    error
+	)
+
+	if err != nil {
+		return "", err
+	}
+
+	return configureAuth(cmd, apiKey)
 }
--- cmd/root.go
+++ cmd/root.go
@@ -34,13 +34,8 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "deepgram",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "The Deepgram command line interface.",
+	Long:  `This is the Deepgram command line interface.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -64,9 +59,9 @@ func init() {
 
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.deepgram-cli.yaml)")
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// loginCmd.Flags().StringP("dg-key", "k", "", "Configure the CLI with your Deepgram API key")
 }
 
 // initConfig reads in config file and ENV variables if set.
