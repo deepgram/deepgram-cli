520b21e3483a18fbed5a098e0f345bcab43c877f
Luke Oliff <luke@lukeoliff.com>
2024-08-05 16:55:20 +0100

    wip: start laying out web auth session creation

--- cmd/login.go
+++ cmd/login.go
@@ -23,10 +23,12 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
+	"deepgram-cli/internal/auth"
 	"deepgram-cli/internal/config"
 	"deepgram-cli/pkg/common"
 )
@@ -44,8 +46,9 @@ func init() {
 	// Allow users to provide an API key directly during login.
 	// This also removes the global scope api_key flag from the --help menu
 	// to reduce confusion and provide a different description in this context.
+	// This isn't bound to the loaded config "api_key" key, so we can tell if a
+	// new key is being provided.
 	loginCmd.Flags().StringVarP(&ApiKey, "api_key", "k", "", "Configure the CLI with your Deepgram API key")
-	viper.BindPFlag("api_key", rootCmd.PersistentFlags().Lookup("api_key"))
 
 	// allow users to force write and skip the prompt
 	loginCmd.Flags().BoolP("force-write", "f", false, "Don't prompt for confirmation when providing an API key")
@@ -54,13 +57,11 @@ func init() {
 func runLogin(cmd *cobra.Command, args []string) {
 	var (
 		// Get the API key from the config context
-		key string = viper.GetString("api_key")
+		key string = cmd.Flags().Lookup("api_key").Value.String()
 
 		str string
 		err error
 	)
-
-	fmt.Println("key", key)
 
 	switch {
 	case key != "":
@@ -73,18 +74,12 @@ func runLogin(cmd *cobra.Command, args []string) {
 }
 
 func cliAuth() error {
-	var err error
-
 	if !viper.GetBool("force-write") {
-		err = common.PromptBool("Do you want to write this key to config?")
+		cobra.CheckErr(common.PromptBool("Do you want to write this key to config?"))
 
 		if config.ConfigFileExists() {
-			err = common.PromptBool("Configuration file already exists. Overwrite?")
+			cobra.CheckErr(common.PromptBool("Configuration file already exists. Overwrite?"))
 		}
-	}
-
-	if err != nil {
-		return err
 	}
 
 	return config.WriteConfigFile()
@@ -92,10 +87,35 @@ func cliAuth() error {
 
 func webAuth() error {
 	var (
-		key string = "123451"
+		key string = viper.GetString("api_key")
+
+		newKey string
+		err    error
 	)
 
-	viper.Set("api_key", key)
+	if key != "" {
+		cobra.CheckErr(common.PromptBool("You're already logged in. Do you want to login again?"))
+	}
+
+	hostname, err := os.Hostname()
+
+	cobra.CheckErr(err)
+
+	fmt.Printf("Logging in to %s\n", hostname)
+
+	session, err := auth.StartSession(hostname)
+	// start session
+	// open browser
+	// wait for response
+
+	fmt.Println(session)
+
+	if err != nil {
+		return err
+	}
+
+	viper.Set("api_key", newKey)
+	newKey = "123456"
 
 	return config.WriteConfigFile()
 }
--- pkg/common/constants.go
+++ pkg/common/constants.go
@@ -5,6 +5,11 @@ const (
 	DefaultConfigText = "(default is $HOME/.config/Deepgram/config.yaml)"
 )
 
+// text consts
+const (
+	DeviceFlowUrl = "https://community.deepgram.com/api/auth/cli/device"
+)
+
 // config consts
 const (
 	ConfigFileName       = "config"
